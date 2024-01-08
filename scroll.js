2fb8edd45e6d3eddf2cd78d3fb83384f027a95ab
mudit108-code <127120092+mudit108-code@users.noreply.github.com>
2024-01-08 12:02:47 +0530

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,30 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let mybutton = document.getElementById("myBtn");
+  const mybutton = document.getElementById("myBtn");
 
-  window.addEventListener("scroll", scrollFunction);
-
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
+  function toggleButtonVisibility() {
+    mybutton.style.display = (window.scrollY > 20) ? "block" : "none";
   }
 
-  mybutton.addEventListener("click", function() {
-    // Check for browser support
-    if ("scrollBehavior" in document.documentElement.style) {
-      // Smooth scroll to the top
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    } else {
-      // Fallback for browsers that do not support smooth scrolling
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    }
-  });
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+  window.addEventListener("scroll", toggleButtonVisibility);
+  mybutton.addEventListener("click", scrollToTop);
 });
